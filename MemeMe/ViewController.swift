374a0e9094fdb6626953b50ed77610d8a0bd620e
Dharini Kadiwala <Dharu@Dharinis-MacBook-Pro.local>
2015-06-18 17:40:38 +0530

    Initial Commit

---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Dharini Kadiwala on 18/06/15.
+//  Copyright (c) 2015 Pritesh Kadiwala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
